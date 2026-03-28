a018201fa5053f367f6051ee4c9230e1cc440322
Rupesh Pradhan <merupeshpradhan@gmail.com>
2026-03-28 15:59:50 +0530

    Little Mongodb connectioon update

--- src/common/config/db.js
+++ src/common/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  const conn = mongoose.connect(process.env.MONGODB_URI);
+  const conn = await mongoose.connect(process.env.MONGODB_URI);
   //what is inside this conn
   console.log(`MongoDB connected: ${conn.connection.host}`);
 };
