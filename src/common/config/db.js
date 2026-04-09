c4daa665dabb90aba463ae1c77884f9ddf08fffd
Rupesh Pradhan <merupeshpradhan@gmail.com>
2026-04-09 09:43:03 +0530

    refactor: comment out unused DNS server configuration and fix method name for notFound

--- src/common/config/db.js
+++ src/common/config/db.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
-import dns from "dns";
+// import dns from "dns";
 
 const connectDB = async () => {
-   dns.setServers(["8.8.8.8", "8.8.4.4"]);
+  // dns.setServers(["8.8.8.8", "8.8.4.4"]);
   const conn = await mongoose.connect(process.env.MONGODB_URI);
   //what is inside this conn
   console.log(`MongoDB connected: ${conn.connection.host}`);
--- src/common/utils/api-error.js
+++ src/common/utils/api-error.js
@@ -19,7 +19,7 @@ class ApiError extends Error {
   static forbidden(message = "forbidden") {
     return new ApiError(412, message);
   }
-  static notfound(message = "notfound") {
+  static notFound(message = "notfound") {
     return new ApiError(412, message);
   }
 }
